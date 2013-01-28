2e87bf631d73af3e69ddb35812c311685370b2b8
James Harris <stoffle@gmail.com>
2013-01-28 22:59:58 +1000

    Added immitations of isolator and asplode.

---
+++ lib/Icecave/Testing/Support/Asplode.php
@@ -0,0 +1,59 @@
+<?php
+namespace Icecave\Testing\Support;
+
+/**
+ * This class is a partial implementation of eloquent/asplode, provided here to prevent
+ * circular dependencies and namespace clashes.
+ *
+ * Please see https://github.com/eloquent/asplode for a usable implementation.
+ */
+class Asplode
+{
+    /**
+     * @return Asplode
+     */
+    public static function instance()
+    {
+        return new static;
+    }
+
+    public function __construct(Isolator $isolator = null)
+    {
+        $this->isolator = Isolator::get($isolator);
+    }
+
+    public function install()
+    {
+        if ($this->installed) {
+            throw new RuntimeException('Already installed.');
+        }
+
+        $this->isolator->set_error_handler(array($this, 'handleError'));
+        $this->installed = true;
+    }
+
+    public function uninstall()
+    {
+        if (!$this->installed) {
+            throw new RuntimeException('Not installed.');
+        }
+
+        $this->isolator->restore_error_handler();
+        $this->installed = false;
+    }
+
+    public function handleError($severity, $message, $filename, $lineno)
+    {
+        throw new \ErrorException($message, 0, $severity, $filename, $lineno);
+    }
+
+    /**
+     * @var boolean
+     */
+    protected $installed = false;
+
+    /**
+     * @var Isolator
+     */
+    protected $isolator;
+}
---
+++ lib/Icecave/Testing/Support/Isolator.php
@@ -0,0 +1,57 @@
+<?php
+namespace Icecave\Testing\Support;
+
+/**
+ * This class is a partial implementation of icecave/isolator, provided here to prevent
+ * circular dependencies and namespace clashes.
+ *
+ * Please see https://github.com/IcecaveStudios/isolator for a usable implementation.
+ */
+class Isolator
+{
+    public static function get(Isolator $instance = NULL)
+    {
+        if ($instance) {
+            return $instance;
+        } elseif (self::$instance) {
+            return self::$instance;
+        } else {
+            return self::$instance = new self;
+        }
+    }
+
+    public function __call($name, array $arguments)
+    {
+        switch ($name) {
+            case 'exit':
+            case 'die':
+                // @codeCoverageIgnoreStart
+                exit(current($arguments));
+                // @codeCoverageIgnoreEnd
+            case 'echo':
+                echo current($arguments);
+                return;
+            case 'eval':
+                return eval(current($arguments));
+            case 'include':
+                return include current($arguments);
+            case 'include_once':
+                return include_once current($arguments);
+            case 'require':
+                return require current($arguments);
+            case 'require_once':
+                return require_once current($arguments);
+            default:
+
+        }
+
+        return call_user_func_array($name, $arguments);
+    }
+
+    public function openssl_public_encrypt($data, &$crypted, $key, $padding = OPENSSL_PKCS1_PADDING)
+    {
+        return openssl_public_encrypt($data, $crypted, $key, $padding);
+    }
+
+    private static $instance;
+}
