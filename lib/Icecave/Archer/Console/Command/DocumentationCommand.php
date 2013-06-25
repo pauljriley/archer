d1acd4be9243993d1c70f461799becde4756a16e
Erin Millard <ezzatron@gmail.com>
2013-06-25 10:59:03 +1000

    Added --open to doc. and coverage commands. Closes #51, closes #52.

--- lib/Icecave/Archer/Console/Command/DocumentationCommand.php
+++ lib/Icecave/Archer/Console/Command/DocumentationCommand.php
@@ -1,20 +1,28 @@
 <?php
 namespace Icecave\Archer\Console\Command;
 
+use Icecave\Archer\Documentation\DocumentationGenerator;
+use Icecave\Archer\Support\Liftoff\Launcher;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Icecave\Archer\Documentation\DocumentationGenerator;
 
 class DocumentationCommand extends Command
 {
-    public function __construct(DocumentationGenerator $generator = null)
-    {
+    public function __construct(
+        DocumentationGenerator $generator = null,
+        Launcher $launcher = null
+    ) {
         if (null === $generator) {
             $generator = new DocumentationGenerator;
         }
+        if (null === $launcher) {
+            $launcher = new Launcher;
+        }
 
         $this->generator = $generator;
+        $this->launcher = $launcher;
 
         parent::__construct();
     }
@@ -27,10 +35,24 @@ class DocumentationCommand extends Command
         return $this->generator;
     }
 
+    /**
+     * @return Launcher
+     */
+    public function launcher()
+    {
+        return $this->launcher;
+    }
+
     protected function configure()
     {
         $this->setName('documentation');
         $this->setDescription('Generate documentation for a project.');
+        $this->addOption(
+            'open',
+            'o',
+            InputOption::VALUE_NONE,
+            'Open the generated documentation in your default web browser.'
+        );
     }
 
     /**
@@ -40,9 +62,16 @@ class DocumentationCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->write('<info>Generating documentation... </info>');
-        $this->generator->generate();
+        $this->generator()->generate();
         $output->writeln('done.');
+
+        if ($input->getOption('open')) {
+            $output->write('<info>Opening documentation... </info>');
+            $this->launcher()->launch('./artifacts/documentation/api/index.html');
+            $output->writeln('done.');
+        }
     }
 
     private $generator;
+    private $launcher;
 }
